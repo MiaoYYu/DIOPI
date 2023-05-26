b03fd0537ad4b212ee28e47cf3248fcd5c1412a2
duanxiang <duanxiang@cambricon.com>
2023-05-26 06:48:15 +0000

    fix format

--- DIOPI-IMPL/camb/functions/split.cpp
+++ DIOPI-IMPL/camb/functions/split.cpp
@@ -14,17 +14,17 @@
 namespace impl {
 namespace camb {
 
-extern "C" diopiError_t diopiSplitWithSizes(diopiContextHandle_t ctx, diopiTensorHandle_t* outs, int64_t num_outs, diopiConstTensorHandle_t input,
+extern "C" diopiError_t diopiSplitWithSizes(diopiContextHandle_t ctx, diopiTensorHandle_t* outs, int64_t numOuts, diopiConstTensorHandle_t input,
                                             const diopiSize_t splitSizes, int64_t dim) {
     cnnlHandle_t handle = cnnlHandlePool.get(ctx);
 
     auto inputTensor = DiopiTensor(input);
     CnnlTensorDesc inputDesc(inputTensor, CNNL_LAYOUT_ARRAY);
 
-    std::vector<CnnlTensorDesc> outputDescs(num_outs);
-    cnnlTensorDescriptor_t* descPtrs = new cnnlTensorDescriptor_t[num_outs];
-    void** dataPtrs = new void*[num_outs];
-    for (int i = 0; i < num_outs; ++i) {
+    std::vector<CnnlTensorDesc> outputDescs(numOuts);
+    cnnlTensorDescriptor_t* descPtrs = new cnnlTensorDescriptor_t[numOuts];
+    void** dataPtrs = new void*[numOuts];
+    for (int i = 0; i < numOuts; ++i) {
         auto tensor = DiopiTensor(outs[i]);
         DIOPI_CALL(outputDescs[i].set(tensor, CNNL_LAYOUT_ARRAY));
         descPtrs[i] = outputDescs[i].get();
@@ -32,14 +32,14 @@ extern "C" diopiError_t diopiSplitWithSizes(diopiContextHandle_t ctx, diopiTenso
     }
 
     size_t worksapceSize;
-    DIOPI_CALLCNNL(cnnlGetSplitWorkspaceSize(handle, num_outs, &worksapceSize));
+    DIOPI_CALLCNNL(cnnlGetSplitWorkspaceSize(handle, numOuts, &worksapceSize));
 
     void* worksapce = nullptr;
     if (worksapceSize != 0) {
         worksapce = requiresBuffer(ctx, worksapceSize).data();
     }
 
-    DIOPI_CALLCNNL(cnnlSplit(handle, num_outs, dim, inputDesc.get(), inputTensor.data(), worksapce, worksapceSize, descPtrs, dataPtrs));
+    DIOPI_CALLCNNL(cnnlSplit(handle, numOuts, dim, inputDesc.get(), inputTensor.data(), worksapce, worksapceSize, descPtrs, dataPtrs));
 
     delete[] descPtrs;
     delete[] dataPtrs;
